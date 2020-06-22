3ff994b33c4a99434146b3cfbb34f987c14fc9ed
summerlikey <summerlikey@163.com>
2020-06-22 17:10:22 +0800

    测试1

--- test/test1.cpp
+++ test/test1.cpp
@@ -8,34 +8,14 @@ using namespace std;
 
 int main()
 {
+    
     int a = 100, b = 99;
+    cout<<a<<"::"<<b<<endl;
     string path = "data";
     string str_id = "1";
     int int_id = 2;
     CreateDirById(path,str_id);
     CreateDirById(path,int_id);
     
-    pcl::PointCloud<pcl::PointXYZ> cloud;
- 
-  // Fill in the cloud data
-    cloud.width    = 5;
-    cloud.height   = 1;
-    cloud.is_dense = false;
-    cloud.points.resize (cloud.width * cloud.height);
- 
-    for (std::size_t i = 0; i < cloud.points.size (); ++i)
-    {
-        cloud.points[i].x = 1024 * rand () / (RAND_MAX + 1.0f);
-        cloud.points[i].y = 1024 * rand () / (RAND_MAX + 1.0f);
-        cloud.points[i].z = 1024 * rand () / (RAND_MAX + 1.0f);
-    }
- 
-    pcl::io::savePCDFileASCII ("build/test_pcd.pcd", cloud);
-    std::cerr << "Saved " << cloud.points.size () << " data points to test_pcd.pcd." << std::endl;
- 
-    for (std::size_t i = 0; i < cloud.points.size (); ++i)
-    std::cerr << "    " << cloud.points[i].x << " " << cloud.points[i].y << " " << cloud.points[i].z << std::endl;
-
-    std::cout<<"hello test"<<a<<":"<<b<<std::endl;
     return 0;
 }
